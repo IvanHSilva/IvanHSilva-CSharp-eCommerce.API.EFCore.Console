582025862576a643f98c2b8372f0c122e3825620
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-07-14 21:50:47 -0300

    Find, First and Last (OrDefault)

--- eCommerce.Queries.Console/Database/eCommerceContext.cs
+++ eCommerce.Queries.Console/Database/eCommerceContext.cs
@@ -4,14 +4,11 @@ using Microsoft.EntityFrameworkCore;
 namespace eCommerce.API.Database {
     public class eCommerceContext : DbContext {
 
-        // Database Connection with configure enviroment
+        // Database Connection without configure enviroment
         string DBConn = "Data Source=SERVIDOR\\SQLSERVER;Initial Catalog=eCommerce;Integrated Security=True;";
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
             optionsBuilder.UseSqlServer(DBConn);
         }
-
-        // Database Connection with configure ennviroment and contructor
-        public eCommerceContext(DbContextOptions<eCommerceContext> options) : base(options) { }
 
         // DBSets
         public DbSet<User> Users { get; set; } = null!;
--- eCommerce.Queries.Console/Program.cs
+++ eCommerce.Queries.Console/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using eCommerce.API.Database;
+using eCommerce.Models;
+
+// Context
+eCommerceContext db = new eCommerceContext()!;
+
+// Select all users
+//List<User> users = db.Users.ToList();
+
+//Console.WriteLine("Usuários:");
+//foreach (User user in users) {
+//    Console.WriteLine($"{user.Id} - {user.Name}");
+//}
+
+//Console.WriteLine();
+Console.WriteLine("Usuário Id 2");
+//User user2 = db.Users!.Find(2);
+User user2 = db.Users!.FirstOrDefault(u => u.Id == 200);
+Console.WriteLine($"{user2!.Id} - {user2.Name}");
+Console.WriteLine();
+Console.WriteLine("Primeiro Usuário");
+User user1 = db.Users!.FirstOrDefault();
+Console.WriteLine($"{user1!.Id} - {user1.Name}");
+Console.WriteLine();
+Console.WriteLine("Último Usuário");
+user1 = db.Users!.OrderBy(u => u.Id).LastOrDefault();
+Console.WriteLine($"{user1!.Id} - {user1.Name}");
+Console.WriteLine();
+Console.WriteLine("Sexo feminino:");
+//List<User> users = db.Users!.Where(u => u.Gender! == "F").OrderBy(u => u.Name).ToList();
+List<User> users = db.Users!.Where(u => u.Gender! == "F").OrderBy(u => u.Name).ToList();
+foreach (User user in users) {
+    Console.WriteLine($"{user.Name}");
+}
